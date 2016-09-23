e6ef017510c9f50c48c9ff9466c1a2dbd6ec540c
tanyaweaver <tanya83@uw.edu>
2016-09-23 14:21:43 -0700

    added production.py

---
+++ production.py
@@ -0,0 +1,10 @@
+from settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+ALLOWED_HOSTS = [
+    'ec2-54-213-216-91.us-west-2.compute.amazonaws.com/',
+    'localhost'
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
